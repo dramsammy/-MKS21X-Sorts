16b05b6079214cbcbd203f35a474018734eb6825
Dramsammy <dramsammy@stuy.edu>
2018-12-05 19:04:14 -0500

    Optimized code, and added test for both positive and negative integers - Finished

--- Sorts.java
+++ Sorts.java
@@ -1,11 +1,9 @@
+import java.util.Arrays;
 public class Sorts{
   public static void selectionsort(int [] ary){
     int min;
     int larger = 0;
     int indexM = 0;
-    int indexL = 0;
-    int current = 0;
-    int counter = 0;
     for (int i = 0; i < ary.length; i++){
       min = ary[i];
       for (int a = i; a < ary.length; a++){
@@ -20,14 +18,11 @@ public class Sorts{
     ary[i] = min;
     }
   }
-}
-//   //TEST
-//   public static void main(String[] args) {
-//     int[] Test = new int[10];
-//     Test = {1,3,5,67,73,2,2,-1,23,-41};
-//     selectionsort(Test);
-//     for (int i = 0; i < Test.length; i++){
-//       System.out.println(Test[i]);
-//     }
-//   }
-// }
+  //TEST
+  public static void main(String[] args) {
+    int[] Test = {1,3,5,67,73,2,2,-1,23,-41};
+    System.out.println(Arrays.toString(Test));
+    selectionsort(Test);
+    System.out.println(Arrays.toString(Test));
+    }
+  }
