29f45a1cb8b00982b4cb8c67fd991fce39ce2717
dramsammy <dramsammy@stuy.edu>
2018-12-07 11:30:29 -0500

    Finished Bubble Sort, will test

--- Sorts.java
+++ Sorts.java
@@ -1,5 +1,9 @@
 import java.util.Arrays;
 public class Sorts{
+  /**Selection sort of an int array.
+ *Upon completion, the elements of the array will be in increasing order.
+ *@param data  the elements to be sorted.
+ */
   public static void selectionsort(int [] ary){
     int min;
     int larger = 0;
@@ -18,6 +22,25 @@ public class Sorts{
     ary[i] = min;
     }
   }
+  /**Bubble sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void bubbleSort(int[] data){
+    int first;
+    int second;
+    for (int i = 0; i <data.length; i++){
+      for (int a = i; a < data.length; a++){
+        first = data[i];
+        second = data[a];
+        if (first > second){
+          data[i] = second;
+          data[a] = first;
+        }
+      }
+    }
+  }
+
   //TEST
   public static void main(String[] args) {
     int[] Test = {1,3,5,67,73,2,2,-1,23,-41};
